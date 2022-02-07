cb671d39eb9a2662382fdd9af4a02b80cc45ad55
Fiorela Ciroku <fiorelaciroku@yahoo.com>
2022-02-07 14:16:12 +0100

    Create crawl-Imports.py

---
+++ .github/script/crawl-Imports.py
@@ -0,0 +1,24 @@
+# Import the SPARQLWrapper package
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+# Declare the wrapper
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+
+# Insert query that is to be executed
+
+sparql.setQuery("""
+    SELECT ?ImportedModule
+    WHERE { 
+       ?module a owl:Ontology .
+       ?ImportedModule a owl:Ontology .
+       ?module owl:imports ?ImportedModule .
+       
+    }
+""")
+# Set return format of the results
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+# Print the results
+for result in results["results"]["bindings"]:
+    print(result["label"]["value"])
